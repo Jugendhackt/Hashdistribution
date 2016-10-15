21d12a01958ec48510e5ce5d14dcbcebdb7d131b
Henning Klatt <henning.klatt@web.de>
2016-10-15 14:50:04 +0200

    Syntax verbessern

--- backend/crawler.py
+++ backend/crawler.py
@@ -19,43 +19,44 @@ def gettweets(hashtag):
         with open(datei) as cachefile:  # Lese Cache aus
             return json.loads(cachefile.read())  # Gebe Cache aus
 
-    try:
-        tso = TwitterSearchOrder()  # Twitter Objekt erstellen
-        tso.set_keywords(['#' + hashtag])  # Wir suchen nach einem hashtag
-        tso.set_language('de')  # Nur Deutsche Tweets
-        tso.set_include_entities(False)  # Kein Entity Zeug ausgeben
+    else: # Wenn kein Cache da ist.
+        try:
+            tso = TwitterSearchOrder()  # Twitter Objekt erstellen
+            tso.set_keywords(['#' + hashtag])  # Wir suchen nach einem hashtag
+            tso.set_language('de')  # Nur Deutsche Tweets
+            tso.set_include_entities(False)  # Kein Entity Zeug ausgeben
 
-        Config = configparser.ConfigParser()
-        if os.path.isfile("config.ini"):
-            Config.read("config.ini")
-        else:
-            print("The config file does not exist, please create a new config with the example file")
-            sys.exit()
+            Config = configparser.ConfigParser()
+            if os.path.isfile("config.ini"):
+                Config.read("config.ini")
+            else:
+                print("The config file does not exist, please create a new config with the example file")
+                sys.exit()
 
-        consumer_key = Config.get("Twitter API", "consumer_key")
-        consumer_secret = Config.get("Twitter API", "consumer_secret")
-        access_token = Config.get("Twitter API", "access_token")
-        access_token_secret = Config.get("Twitter API", "access_token_secret")
+            consumer_key = Config.get("Twitter API", "consumer_key")
+            consumer_secret = Config.get("Twitter API", "consumer_secret")
+            access_token = Config.get("Twitter API", "access_token")
+            access_token_secret = Config.get("Twitter API", "access_token_secret")
 
-        # Objekt mit Zugangsdaten erstellen
-        ts = TwitterSearch(
-            consumer_key=consumer_key,
-            consumer_secret=consumer_secret,
-            access_token=access_token,
-            access_token_secret=access_token_secret
-        )
+            # Objekt mit Zugangsdaten erstellen
+            ts = TwitterSearch(
+                consumer_key=consumer_key,
+                consumer_secret=consumer_secret,
+                access_token=access_token,
+                access_token_secret=access_token_secret
+                )
 
-        tweets = set()
-        for tweet in ts.search_tweets_iterable(tso):
-            tweets.add(tweet['text'])
+            tweets = set()
+            for tweet in ts.search_tweets_iterable(tso):
+                tweets.add(tweet['text'])
 
-        tweets = list(tweets)
+            tweets = list(tweets)
 
-        with open(datei, 'w') as cachefile:
-            tweetsasjson = json.dumps(tweets)
-            cachefile.write(tweetsasjson)
+            with open(datei, 'w') as cachefile:
+                tweetsasjson = json.dumps(tweets)
+                cachefile.write(tweetsasjson)
 
-        return tweets
+            return tweets
 
-    except TwitterSearchException as e:  # take care of all those ugly errors if there are some
-        print(e)
+        except TwitterSearchException as e:  # take care of all those ugly errors if there are some
+            print(e)
