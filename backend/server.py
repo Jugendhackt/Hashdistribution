b8d3b7cb49fdf82994504bb73e6c781a5f72b4ca
Henning Klatt <henning.klatt@web.de>
2016-10-15 19:36:53 +0200

    Testen

--- backend/server.py
+++ backend/server.py
@@ -10,6 +10,10 @@ def all():
     depth = request.args.get('depth')
     return '{0}({1})'.format(callback, {'client_ip':ip,'hastag':hastag,'depth':depth})
 
+@app.route('/debug')
+def debug():
+    ip = request.remote_addr
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
