0ef8b942d4854179ff3331ab02abfbac812001f7
Henning Klatt <henning.klatt@web.de>
2016-10-15 15:23:56 +0200

    Weitere Debüt Infos

--- backend/crawler.py
+++ backend/crawler.py
@@ -21,7 +21,8 @@ def gettweets(hashtag):
     if (os.path.isfile(datei) and chachetime(datei)):  # Wenn Cache existiert
         with open(datei) as cachefile:  # Lese Cache aus
             print("Ausgabe aus Cache " + datei)
-            print("Cache wurde zuletzt vor " + str(round(daysDiff / 3600, 2)) + " Stunde/n aktualisiert")
+            print("Cache wurde zuletzt vor " + str(round(daysDiff / 3600, 2)) + " Stunde/n aktualisiert.")
+            print("Nächste Aktualisierung in: " + str(100-(round(daysDiff / 3600, 2))) + " Stunden."
             return json.loads(cachefile.read())  # Gebe Cache aus
 
     try:
