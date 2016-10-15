3ed37f19e26d22de286ef198da3af7e3130b44c9
Fronbasal <mail@fronbasal.de>
2016-10-15 21:58:20 +0200

    Fixed some data issues. Fy pi.

--- frontend/Landing/index.php
+++ frontend/Landing/index.php
@@ -43,6 +43,7 @@
 <div id="bgImg" class="container"></div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="https://d3js.org/d3.v4.min.js"></script>
 <script src="js/app.js"></script>
 </body>
 </html>
