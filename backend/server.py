47b6fcec81511fcb37bc6add8d7e4c2799e71213
Henning Klatt <henning.klatt@web.de>
2016-10-16 02:38:38 +0200

    callback entfernen

--- backend/server.py
+++ backend/server.py
@@ -49,7 +49,6 @@ def set_allow_origin(resp):
 def all():
     #response.headers.add('Access-Control-Allow-Origin', '*')
     ip = request.remote_addr
-    callback = request.args.get('callback')
     hashtag = request.args.get('hashtag')
     depth = request.args.get('depth')
     # crawler.getjson(hashtag)
@@ -59,8 +58,7 @@ def all():
     else:
         return json.dumps('{ error:"Please add the arguments hashtag and depth" }')
 
-    callback = request.args.get('callback')
-    return '{0}({1})'.format(callback, Json)
+    return Json
 
 
 @app.route('/debug')
