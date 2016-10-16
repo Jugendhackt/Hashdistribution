df67a574be4068d4bd9ef27d3821be1bc4e65fed
fronbasal <mail@fronbasal.de>
2016-10-16 12:39:16 +0200

    After our horrible presentation

---
+++ frontend/js/app.js
@@ -0,0 +1,51 @@
+function b_press() {
+    window.location = "make/display?hashtag=" + $("#querybox").val();
+}
+$(document).ready(function () {
+    init_bg();
+    b();
+    check();
+    $("#sizing-addon1").click(function () {
+        check();
+    });
+});
+function check() {
+    $(window).on("keydown", function (e) {
+        var el = $("input:text").val();
+        if (el.length != 0) {
+            if (e.keyCode == 13) {
+                if (~el.indexOf(" ")) {
+                    $("#handle").removeClass("hide");
+                } else {
+                    !$("#handle").hasClass("hide") ? $("#handle").addClass("hide") : a();
+                    b_press();
+                }
+
+            }
+        }
+    });
+}
+function b() {
+    $("#bgImg").animate({"top":"-10000px"}, 200000, "linear");
+    var scrollPosition = [ self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,  self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop];
+    var html = jQuery('html');
+    html.data('scroll-position', scrollPosition);
+    html.data('previous-overflow', html.css('overflow'));
+    html.css('overflow', 'hidden');
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+}
+function a() {
+}
+function init_bg() {
+    $.ajax({
+        url: "data/jh.json",
+        dataType: "JSON",
+        method: "GET",
+        success: function (response) {
+            response.forEach(function (data) {
+                var el1 = $("#bgImg");
+                ~data.indexOf("RT") ? el1.html(el1.html() + "<p class='item'><span class='glyphicon glyphicon-send'></span>" + data.replace("RT", '  ') + "</p><br/>") : el1.html(el1.html() + "<p class='item'>" + data + "</p><br/>");
+            });
+        }
+    });
+}
